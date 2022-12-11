8a64199abf426d9ec214dad7e30c0e57cd35ea1f
ashik iqbal emon <91707003+aiemond@users.noreply.github.com>
2022-12-11 18:15:37 +0100

    Add files via upload

---
+++ matrixFODO.py
@@ -0,0 +1,198 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov 29 10:04:58 2022
+
+@author: emondemoniac
+"""
+
+import os
+
+import numpy as np
+#import matplotlib.pyplot as plt
+#import matplotlib.patches as mpatches
+#from scipy.interpolate import interp1d
+
+
+class Component:
+    def __init__(self, typ, name, length, strength, param=0):
+        self.length = length
+        self.strength = strength
+        self.param = param
+        self.name = name
+        self.typ = typ.lower()
+
+        if self.typ == 'qf':
+            self.mat = self.m_qf(self.length, self.strength)
+
+        if self.typ == 'qd':
+            self.mat = self.m_qd(self.length, self.strength)
+
+        if self.typ == 'b':
+            self.mat = self.m_b(self.length, self.strength)
+
+        if self.typ == 'eb':
+            self.mat = self.m_eb(self.strength, self.param)
+
+        if self.typ == 'd':
+            self.mat = self.m_d(self.length)
+
+    def m_d(self, s):
+        return np.array([[1, s, 0, 0, 0],
+                         [0, 1, 0, 0, 0],
+                         [0, 0, 1, s, 0],
+                         [0, 0, 0, 1, 0],
+                         [0, 0, 0, 0, 1]])
+
+    def m_qf(self, s, k):
+        if k >= 0:
+            return np.zeros((5, 5))
+        sq_abs_k = np.sqrt(np.abs(k))
+        Omega = sq_abs_k * s
+        return np.array([[np.cos(Omega), np.sin(Omega) / sq_abs_k, 0, 0, 0],
+                         [-sq_abs_k * np.sin(Omega), np.cos(Omega), 0, 0, 0],
+                         [0, 0, np.cosh(Omega), np.sinh(Omega) / sq_abs_k, 0],
+                         [0, 0, sq_abs_k * np.sinh(Omega), np.cosh(Omega), 0],
+                         [0, 0, 0, 0, 1]])
+
+    def m_qd(self, s, k):
+        if k <= 0:
+            return np.zeros((5, 5))
+        sq_abs_k = np.sqrt(np.abs(k))
+        Omega = sq_abs_k * s
+        return np.array([[np.cosh(Omega), np.sinh(Omega) / sq_abs_k, 0, 0, 0],
+                         [sq_abs_k * np.sinh(Omega), np.cosh(Omega), 0, 0, 0],
+                         [0, 0, np.cos(Omega), np.sin(Omega) / sq_abs_k, 0],
+                         [0, 0, -sq_abs_k * np.sin(Omega), np.cos(Omega), 0],
+                         [0, 0, 0, 0, 1]])
+
+    def m_b(self, s, R):
+        if R <= 0:
+            return np.zeros((5, 5))
+        chi = s / R
+        return np.array([[np.cos(chi), R * np.sin(chi), 0, 0, R * (1 - np.cos(chi))],
+                         [-np.sin(chi) / R, np.cos(chi), 0, 0, np.sin(chi)],
+                         [0, 0, 1, s, 0],
+                         [0, 0, 0, 1, 0],
+                         [0, 0, 0, 0, 1]])
+
+    def m_eb(self, R, psi):
+        if R <= 0:
+            return np.zeros((5, 5))
+        psi = np.deg2rad(psi)
+        return np.array([[1, 0, 0, 0, 0],
+                         [np.tan(psi) / R, 1, 0, 0, 0],
+                         [0, 0, 1, 0, 0],
+                         [0, 0, -np.tan(psi) / R, 1, 0],
+                         [0, 0, 0, 0, 1]])
+
+
+class Cell:
+    def __init__(self, filename):
+
+        self.blau = '#3090C7'
+        self.gruen = '#507642'
+        self.rot = '#D23641'
+
+        self.filename = filename
+        with open(self.filename) as f:
+            self.title = f.readline().replace('#', '')
+        lat = np.genfromtxt(filename, dtype=None)
+        self.component_list = []
+        self.length_list = []
+        self.component_number = np.size(lat)
+        self.length_list_sum = [0]
+
+        for i in range(np.size(lat)):
+            if np.size(lat) == 1:
+                c = Component(lat[()][0].decode("utf-8"), lat[()][1], lat[()][2], lat[()][3], lat[()][4])
+            else:
+                c = Component(lat[i][0].decode("utf-8"), lat[i][1], lat[i][2], lat[i][3], lat[i][4])
+            self.component_list.append(c)
+            self.length_list.append(c.length)
+            self.length_list_sum.append(self.length_list_sum[i] + c.length)
+
+
+    def beta_a_x(self, m):
+        return np.sqrt(-(m[0][1] * m[1][1]) / (m[1][0] * m[0][0]))
+
+    def beta_b_x(self, m):
+        return -1 / self.beta_a_x(m) * m[0][1] / m[1][0]
+
+    def beta_a_y(self, m):
+        return np.sqrt(-(m[2][3] * m[3][3]) / (m[3][2] * m[3][3]))
+
+    def beta_b_y(self, m):
+        return -1 / self.beta_a_y(m) * m[2][3] / m[3][2]
+
+    def d_a_x(self, m):
+        return -m[1][4] / m[1][0]
+
+    def d_a_y(self, m):
+        return -m[0][0] * m[1][4] / m[1][0] + m[0][4]
+
+    def total_matrix(self):
+        m = np.identity(5)
+        for i in range(len(self.component_list)):
+            m = np.dot(m, self.component_list[i].mat)
+        return m
+
+    def make_unique(self, seq):
+        seen = set()
+        seen_add = seen.add
+        return [x for x in seq if not (x in seen or seen_add(x))]
+
+    def evolve_beta(self):
+        Mcell = self.total_matrix()
+        beta_x_0 = self.beta_a_x(Mcell)
+        beta_y_0 = self.beta_a_y(Mcell)
+        alpha_x_0 = 0
+        alpha_y_0 = 0
+        gamma_x_0 = (1 + alpha_x_0 ** 2) / beta_x_0
+        gamma_y_0 = (1 + alpha_y_0 ** 2) / beta_y_0
+
+        B0 = np.array([[beta_x_0, -alpha_x_0, 0, 0, 0],
+                       [-alpha_x_0, gamma_x_0, 0, 0, 0],
+                       [0, 0, beta_y_0, -alpha_y_0, 0],
+                       [0, 0, -alpha_y_0, gamma_y_0, 0],
+                       [0, 0, 0, 0, 1]])
+
+        print("BETA {}, {}".format(beta_x_0, beta_y_0))
+        number = len(self.component_list)
+        beta_x_list, beta_y_list, position_list = [0] * (number + 1), [0] * (number + 1), [0] * (number + 1)
+
+
+        beta_x_list[0] = self.beta_a_x(Mcell)
+        beta_y_list[0] = self.beta_a_y(Mcell)
+        beta_x_list[number] = self.beta_b_x(Mcell)
+        beta_y_list[number] = self.beta_b_y(Mcell)
+
+        position = 0
+        position_list[0] = position
+
+        B = B0  
+
+        for i in range(number):
+            M = self.component_list[i].mat
+            print('')
+            print(self.component_list[i].name)
+            print("Before Matrix: Position: {}, beta_x: {}".format(position_list[i],
+                                                                   beta_x_list[i]))
+            print(B)
+            print('')
+            print(M)
+            position += self.component_list[i].length
+            B = np.dot(np.dot(M, B), M.T)
+            beta_x_list[i + 1] = B[0][0]
+            beta_y_list[i + 1] = B[2][2]
+            position_list[i + 1] = position
+            print("After Matrix: Position: {}, beta_x: {}".format(position_list[i + 1],
+                                                                  beta_x_list[i + 1]))
+
+        return position_list, beta_x_list, beta_y_list
+    
+if __name__ == "__main__":
+    c = Cell('willey1.txt')
+    print(c.total_matrix())
+    c.evolve_beta()
+   
